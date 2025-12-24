df00f83eba98d0a67ee658dcb25911cd37a12269
Chayne <chaynesinghwhite@gmail.com>
2025-12-24 15:08:53 -0500

    testing preloading imging for efficiency.

--- script.js
+++ script.js
@@ -1,19 +1,219 @@
+// import gsap from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { SplitText } from "gsap/SplitText";
+// import Lenis from "lenis";
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+// 	gsap.registerPlugin(ScrollTrigger, SplitText);
+
+// 	const lenis = new Lenis();
+
+// 	lenis.on("scroll", ScrollTrigger.update); 
+// 	gsap.ticker.add((time) => {
+// 		lenis.raf(time * 1000);
+// 	});
+
+// 	gsap.ticker.lagSmoothing(0);
+
+// 	const slides = [
+// 		{
+// 			title: " 1 testing 1, 3, nah test 2 if i was you...",
+// 			image: "/images/serene-monochromatic-landscape.png",
+// 		},
+// 		{
+// 			title: " 2 testing 1, 3, nah test 2 if i was you...",
+// 			image: "/images/desert-pink-moon.png",
+// 		},
+// 		{
+// 			title: " 3 testing 1, 3, nah test 2 if i was you...",
+// 			image: "/images/roman-unsplash.jpg",
+// 		},
+// 		{
+// 			title: " 4 testing 1, 3, nah test 2 if i was you...",
+// 			image: "/images/pose-japan.jpg",
+// 		},
+// 		{
+// 			title: " 5 testing 1, 3, nah test 2 if i was you...",
+// 			image: "/images/skeletal-figure.png",
+// 		},
+// 		{
+// 			title: " 6 testing 1, 3, nah test 2 if i was you...",
+// 			image: "/images/battle-japan.jpg",
+// 		},
+// 		{
+// 			title: " 7 testing 1, 3, nah test 2 if i was you...",
+// 			image: "/images/pose-japan.jpg",
+// 		},
+// 	];
+
+// 	const pinDistance = window.innerHeight * slides.length;
+// 	const progressBar = document.querySelector(".slider-progress");
+// 	const sliderImages = document.querySelector(".slider-images");
+// 	const sliderTitle = document.querySelector(".slider-title");
+// 	const sliderIndices = document.querySelector(".slider-indices");
+
+// 	let activeSlide = 0;
+// 	let currentSplit = null;
+
+// 	function createIndices() {
+// 		sliderIndices.innerHTML = "";
+
+// 		slides.forEach((_, index) => {
+// 			const indexNum = (index + 1).toString().padStart(2, "0");
+// 			const indicatorElement = document.createElement("p");
+// 			indicatorElement.dataset.index = index;
+// 			indicatorElement.innerHTML = `<span class="marker"></span><span class="index">${indexNum}</span>`
+			
+// 			sliderIndices.appendChild(indicatorElement);
+
+// 			if (index === 0) {
+// 				gsap.set(indicatorElement.querySelector(".index"), {
+// 					opacity: 1,
+// 				});
+// 				gsap.set(indicatorElement.querySelector(".marker"), {
+// 					scaleX: 1,
+// 				});
+// 			} else {
+// 				gsap.set(indicatorElement.querySelector(".index"), {
+// 					opacity: 0.35,
+// 				});
+// 				gsap.set(indicatorElement.querySelector(".marker"), {
+// 					scaleX: 0,
+// 				});
+// 			}
+
+
+// 		});
+// 	}
+
+// 	function animateNewSlide(index) {
+// 		const newSliderImage = document.createElement("img");
+// 		newSliderImage.src = slides[index].image;
+// 		newSliderImage.alt = `Slide ${index + 1}`;
+
+// 		gsap.set(newSliderImage, {
+// 			opacity: 0,
+// 			scale: 1.1,
+// 		});
+
+// 		sliderImages.appendChild(newSliderImage);
+
+// 		gsap.to(newSliderImage, {
+// 			opacity: 1,
+// 			duration: 0.5,
+// 			ease: "power2.out",
+// 		});
+
+// 		const allImages = sliderImages.querySelectorAll("img");
+		
+// 		if (allImages.length > 3) {
+// 			const removeCount = allImages.length - 3;
+// 			for (let i = 0; i < removeCount; i++) {
+// 				sliderImages.removeChild(allImages[i]);
+// 			}
+// 		}
+
+// 		animateNewTitle(index);
+// 		animateIndicators(index);
+// 	}
+
+
+// 	function animateIndicators(index) {
+// 		const indicators = sliderIndices.querySelectorAll("p");
+
+// 		indicators.forEach((indicator, i) => {
+// 			const markerElement = indicator.querySelector(".marker");
+// 			const indexElement = indicator.querySelector(".index");
+// 			if (i === index) {
+// 				gsap.to(indexElement, {
+// 					opacity: 1,
+// 					duration: 0.3,
+// 					ease: "power2.out",
+// 				});
+
+// 				gsap.to(markerElement, {
+// 					scaleX: 1,
+// 					duration: 0.3,
+// 					ease: "power2.out",
+// 				});
+// 			} else {
+// 				gsap.to(indexElement, {
+// 					opacity: 0.5,
+// 					duration: 0.3,
+// 					ease: "power2.out",
+// 				});
+// 			}
+// 		});
+// 	}
+
+// 	function animateNewTitle(index) {
+
+// 		if (currentSplit) {
+// 			currentSplit.revert();
+// 		}
+
+// 		sliderTitle.innerHTML = `<h1>${slides[index].title}</h1>`;
+
+// 		currentSplit = new SplitText(sliderTitle.querySelector("h1"), {
+// 			type: "lines",
+// 			lineClass: "line",
+// 			mask: "lines",
+// 		});
+
+// 		gsap.set(currentSplit.lines, {
+// 			yPercent: 100,
+// 			opacity: 0,
+// 		});
+
+// 		gsap.to(currentSplit.lines, {
+// 			yPercent: 0,
+// 			opacity: 1,
+// 			duration: 0.75,
+// 			stagger: 0.1,
+// 			ease: "power3.out",
+// 		});
+
+// 	} //end
+
+// 	createIndices();
+
+// 	ScrollTrigger.create({
+// 		trigger: ".slider",
+// 		start: "top top",
+// 		end: `+=${pinDistance}px`,
+// 		scrub: 1,
+// 		pin: true,
+// 		pinSpacing: true,
+// 		onUpdate: (self) => {
+// 			gsap.set(progressBar, {
+// 				scaleY: self.progress,
+// 			});
+
+// 			const currentSlide = Math.floor(self.progress * slides.length);
+
+// 			if (activeSlide !== currentSlide && currentSlide < slides.length) {
+// 				activeSlide = currentSlide;
+// 				animateNewSlide(activeSlide);
+// 			}
+// 		},
+// 	});
+
+// });
+
+
+
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 import Lenis from "lenis";
 
-
 document.addEventListener("DOMContentLoaded", () => {
 	gsap.registerPlugin(ScrollTrigger, SplitText);
 
 	const lenis = new Lenis();
-
-	lenis.on("scroll", ScrollTrigger.update); 
-	gsap.ticker.add((time) => {
-		lenis.raf(time * 1000);
-	});
-
+	lenis.on("scroll", ScrollTrigger.update);
+	gsap.ticker.add((time) => lenis.raf(time * 1000));
 	gsap.ticker.lagSmoothing(0);
 
 	const slides = [
@@ -55,152 +255,122 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	let activeSlide = 0;
 	let currentSplit = null;
+	let preloadedImages = [];
+
+	function preloadImages(slides) {
+		return Promise.all(
+			slides.map((slide) => {
+				return new Promise((resolve, reject) => {
+					const img = new Image();
+					img.src = slide.image;
+					img.onload = () => resolve(img);
+					img.onerror = reject;
+				});
+			})
+		);
+	}
 
 	function createIndices() {
 		sliderIndices.innerHTML = "";
 
 		slides.forEach((_, index) => {
 			const indexNum = (index + 1).toString().padStart(2, "0");
-			const indicatorElement = document.createElement("p");
-			indicatorElement.dataset.index = index;
-			indicatorElement.innerHTML = `<span class="marker"></span><span class="index">${indexNum}</span>`
-			
-			sliderIndices.appendChild(indicatorElement);
+			const indicator = document.createElement("p");
+			indicator.innerHTML = `<span class="marker"></span><span class="index">${indexNum}</span>`;
+			sliderIndices.appendChild(indicator);
 
-			if (index === 0) {
-				gsap.set(indicatorElement.querySelector(".index"), {
-					opacity: 1,
-				});
-				gsap.set(indicatorElement.querySelector(".marker"), {
-					scaleX: 1,
-				});
-			} else {
-				gsap.set(indicatorElement.querySelector(".index"), {
-					opacity: 0.35,
-				});
-				gsap.set(indicatorElement.querySelector(".marker"), {
-					scaleX: 0,
-				});
-			}
-
-
+			gsap.set(indicator.querySelector(".index"), {
+				opacity: index === 0 ? 1 : 0.35,
+			});
+			gsap.set(indicator.querySelector(".marker"), {
+				scaleX: index === 0 ? 1 : 0,
+			});
 		});
 	}
 
 	function animateNewSlide(index) {
-		const newSliderImage = document.createElement("img");
-		newSliderImage.src = slides[index].image;
-		newSliderImage.alt = `Slide ${index + 1}`;
+		const newImage = preloadedImages[index].cloneNode();
+		newImage.alt = `Slide ${index + 1}`;
 
-		gsap.set(newSliderImage, {
-			opacity: 0,
-			scale: 1.1,
-		});
+		gsap.set(newImage, { opacity: 0, scale: 1.1 });
+		sliderImages.appendChild(newImage);
 
-		sliderImages.appendChild(newSliderImage);
-
-		gsap.to(newSliderImage, {
+		gsap.to(newImage, {
 			opacity: 1,
 			duration: 0.5,
 			ease: "power2.out",
 		});
 
-		const allImages = sliderImages.querySelectorAll("img");
-		
-		if (allImages.length > 3) {
-			const removeCount = allImages.length - 3;
-			for (let i = 0; i < removeCount; i++) {
-				sliderImages.removeChild(allImages[i]);
-			}
+		const images = sliderImages.querySelectorAll("img");
+		if (images.length > 3) {
+			images[0].remove();
 		}
 
 		animateNewTitle(index);
 		animateIndicators(index);
 	}
 
-
 	function animateIndicators(index) {
 		const indicators = sliderIndices.querySelectorAll("p");
 
-		indicators.forEach((indicator, i) => {
-			const markerElement = indicator.querySelector(".marker");
-			const indexElement = indicator.querySelector(".index");
-			if (i === index) {
-				gsap.to(indexElement, {
-					opacity: 1,
-					duration: 0.3,
-					ease: "power2.out",
-				});
-
-				gsap.to(markerElement, {
-					scaleX: 1,
-					duration: 0.3,
-					ease: "power2.out",
-				});
-			} else {
-				gsap.to(indexElement, {
-					opacity: 0.5,
-					duration: 0.3,
-					ease: "power2.out",
-				});
-			}
+		indicators.forEach((el, i) => {
+			gsap.to(el.querySelector(".index"), {
+				opacity: i === index ? 1 : 0.5,
+				duration: 0.3,
+			});
+			gsap.to(el.querySelector(".marker"), {
+				scaleX: i === index ? 1 : 0,
+				duration: 0.3,
+			});
 		});
 	}
 
 	function animateNewTitle(index) {
-
-		if (currentSplit) {
-			currentSplit.revert();
-		}
+		if (currentSplit) currentSplit.revert();
 
 		sliderTitle.innerHTML = `<h1>${slides[index].title}</h1>`;
-
 		currentSplit = new SplitText(sliderTitle.querySelector("h1"), {
 			type: "lines",
 			lineClass: "line",
 			mask: "lines",
 		});
 
-		gsap.set(currentSplit.lines, {
-			yPercent: 100,
-			opacity: 0,
-		});
-
-		gsap.to(currentSplit.lines, {
-			yPercent: 0,
-			opacity: 1,
-			duration: 0.75,
-			stagger: 0.1,
-			ease: "power3.out",
-		});
-
-	} //end
-
-	createIndices();
-
-	ScrollTrigger.create({
-		trigger: ".slider",
-		start: "top top",
-		end: `+=${pinDistance}px`,
-		scrub: 1,
-		pin: true,
-		pinSpacing: true,
-		onUpdate: (self) => {
-			gsap.set(progressBar, {
-				scaleY: self.progress,
-			});
-
-			const currentSlide = Math.floor(self.progress * slides.length);
-
-			if (activeSlide !== currentSlide && currentSlide < slides.length) {
-				activeSlide = currentSlide;
-				animateNewSlide(activeSlide);
+		gsap.fromTo(
+			currentSplit.lines,
+			{ yPercent: 100, opacity: 0 },
+			{
+				yPercent: 0,
+				opacity: 1,
+				duration: 0.75,
+				stagger: 0.1,
+				ease: "power3.out",
 			}
-		},
+		);
+	}
+
+	function initScrollTrigger() {
+		ScrollTrigger.create({
+			trigger: ".slider",
+			start: "top top",
+			end: `+=${pinDistance}px`,
+			scrub: 1,
+			pin: true,
+			onUpdate: (self) => {
+				gsap.set(progressBar, { scaleY: self.progress });
+
+				const currentSlide = Math.floor(self.progress * slides.length);
+				if (activeSlide !== currentSlide && currentSlide < slides.length) {
+					activeSlide = currentSlide;
+					animateNewSlide(activeSlide);
+				}
+			},
+		});
+	}
+
+	preloadImages(slides).then((images) => {
+		preloadedImages = images;
+		createIndices();
+		initScrollTrigger();
 	});
-
 });
-
-
-
 
